e8faf12874795b836bc0950cf6a16aa91e84d4ba
Sree K <SreeKanak@Srees-MacBook-Pro.local>
2014-07-04 20:56:37 +1000

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Sree K on 4/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end

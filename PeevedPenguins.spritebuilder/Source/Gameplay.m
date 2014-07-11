d536c6673eb9143d28ab01271d629003e4b7c883
Sree K <SreeKanak@Srees-MacBook-Pro.local>
2014-07-11 23:29:02 +1000

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -8,6 +8,7 @@
 
 #import "Gameplay.h"
 #import "CCPhysics+ObjectiveChipmunk.h"
+#import "Penguin.h"
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
@@ -16,8 +17,10 @@
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
-    CCNode *_currentPenguin;
+    Penguin *_currentPenguin;
     CCPhysicsJoint *_penguinCatapultJoint;
+    static const float MIN_SPEED = 5.f;
+    CCAction *_followPenguin;
 }
 
 // is called when CCB file has completed loading
@@ -36,7 +39,28 @@
     
     
 }
-
+- (void)update:(CCTime)delta
+{if (_currentPenguin.launched)
+    // if speed is below minimum speed, assume this attempt is over
+    if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMin = _currentPenguin.boundingBox.origin.x;
+    
+    if (xMin < self.boundingBox.origin.x) {
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMax = xMin + _currentPenguin.boundingBox.size.width;
+    
+    if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
+        [self nextAttempt];
+        return;
+    }
+}
 // called on every touch in this scene
 -(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
@@ -45,7 +69,7 @@
     // start catapult dragging when a touch inside of the catapult arm occurs
     if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
     {// create a penguin from the ccb-file
-        _currentPenguin = [CCBReader load:@"Penguin"];
+        _currentPenguin = (Penguin*)[CCBReader load:@"Penguin"];
         // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
         CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
         // transform the world position to the node space to which the penguin will be added (_physicsNode)
@@ -70,6 +94,7 @@
         _mouseJointNode.position = touchLocation;
     }
     - (void)releaseCatapult {
+        _currentPenguin.launched = TRUE;
         if (_mouseJoint != nil)
         {
             // releases the joint and lets the penguin fly
@@ -80,9 +105,8 @@
             _currentPenguin.physicsBody.allowsRotation = TRUE;
             
             // follow the flying penguin
-            CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
-            [_contentNode runAction:follow];
-            // releases the joint and lets the catapult snap back
+            _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+            [_contentNode runAction:_followPenguin];            // releases the joint and lets the catapult snap back
             [_mouseJoint invalidate];
             _mouseJoint = nil;
         }
@@ -146,6 +170,12 @@
     // finally, remove the destroyed seal
     [seal removeFromParent];
 }
-
+- (void)nextAttempt {
+    _currentPenguin = nil;
+    [_contentNode stopAction:_followPenguin];
+    
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0, 0)];
+    [_contentNode runAction:actionMoveTo];
+}
 
 @end

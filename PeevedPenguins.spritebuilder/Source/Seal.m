07a9f582cf0e90a7bd967e0011edbca66ca444f0
Sree K <SreeKanak@Srees-MacBook-Pro.local>
2014-07-11 17:40:21 +1000

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Seal.m
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -7,14 +7,26 @@
 //
 
 #import "Seal.h"
+#import "CCPhysics+ObjectiveChipmunk.h"
 
 @implementation Seal
 -(void)didLoadFromCCB {
     self.physicsBody.collisionType = @"seal";
+    _physicsNode.collisionDelegate = self;
 }
--(void) ccPhysicsCollisionPostSolive:(CCPhysicsCollisionPair *)pair seal:(CCNode *) nodeA wildcard:(CCNode *)node;
-{
-    CCLOG(@"Something collided with seal");
+- (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB {
+    float energy = [pair totalKineticEnergy];
     
+    // if energy is large enough, remove the seal
+    if (energy > 5000.f) {
+        [[_PhysicsNode space] addPostStepBlock:^{
+            [self sealRemoved:nodeA];
+        } key:nodeA];
+    }
 }
+
+- (void)sealRemoved:(CCNode *)seal {
+    [seal removeFromParent];
+}
+
 @end

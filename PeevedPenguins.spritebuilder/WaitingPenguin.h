9bdefc3aef5770ef2896ba10dabdc841a41c6093
Sree K <SreeKanak@Srees-MacBook-Pro.local>
2014-07-11 21:54:12 +1000

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Sree K on 11/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end

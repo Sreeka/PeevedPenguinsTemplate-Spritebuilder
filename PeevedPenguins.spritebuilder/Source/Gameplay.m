da247d4a821e630d58b7222b22c650bff21f6381
Sree K <SreeKanak@Srees-MacBook-Pro.local>
2014-07-11 23:45:50 +1000

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -43,7 +43,7 @@
 
 {if (_currentPenguin.launched)
     // if speed is below minimum speed, assume this attempt is over
-    if (ccpLength(_currentPenguin.physicsBody.velocity) < 8.f){
+    if (ccpLength(_currentPenguin.physicsBody.velocity) < 12.f){
         [self nextAttempt];
         return;
     }
